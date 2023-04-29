9a6635682e74c8771e35f5900be277375946605c
AnatoliyVinokurov <anatolij.isit@gmail.com>
2023-04-29 17:36:51 +0300

    added toaster on scene

--- core/src/com/anatoliyvinokurov/flyingtoaster/FlyingToaster.java
+++ core/src/com/anatoliyvinokurov/flyingtoaster/FlyingToaster.java
@@ -11,7 +11,17 @@ public class FlyingToaster extends ApplicationAdapter {
 	Texture background;
 	Texture[] birds;
 	int flapState = 0;
-	
+	float birdY = 0;
+	float velocity = 0;
+
+	int gameState = 0;
+
+	float gravity = 2;
+
+	Texture topTube;
+	Texture bottomTube;
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -19,23 +29,46 @@ public class FlyingToaster extends ApplicationAdapter {
 		birds = new Texture[2];
 		birds[0] = new Texture("bird.png");
 		birds[1] = new Texture("bird2.png");
+		birdY = Gdx.graphics.getHeight()/2-birds[0].getHeight()/2;
+
+
 	}
 
 	@Override
 	public void render () {
 
-		if(flapState == 0){
+		if(gameState != 0) {
+
+			if(Gdx.input.justTouched()) {
+				velocity = -30;
+			}
+
+			if(birdY > 0 || velocity < 0){
+				velocity = velocity + gravity;
+				birdY -= velocity;
+			}
+
+
+
+		}else{
+			if(Gdx.input.justTouched()) {
+				gameState = 1;
+			}
+
+		}
+
+		if (flapState == 0) {
 			flapState = 1;
-		}else {
+		} else {
 			flapState = 0;
 		}
 
 		batch.begin();
-		batch.draw(background, 0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(birds[flapState],Gdx.graphics.getWidth()/2 - birds[flapState].getWidth()/2, Gdx.graphics.getHeight()/2-birds[flapState].getHeight()/2);
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2, birdY);
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
